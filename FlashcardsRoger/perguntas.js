431fcc5a28b97581a0a56a4b325c646140bfb74d
ValuingBoat <150253824+ValuingBoat@users.noreply.github.com>
2024-11-22 10:31:34 -0300

    Add files via upload
    22/11/24

---
+++ FlashcardsRoger/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Quem foi a Primeira Programadora?',
+    'A primeira programadora da história foi Ada Lovelace, que trabalhou no século 19 com a máquina analítica de Charles Babbage.'
+)
+
+criaCartao(
+    'Geografia',
+    'O Monte Everest está crecendo?',
+    'A montanha mais alta do mundo, continua a crescer cerca de 4 milímetros por ano devido à tectônica de placas. Isso significa que, embora sua altura oficial seja de 8.848 metros, ela está sempre mudando!'
+)
+
+criaCartao(
+    'Programação',
+    'De onde surgiu o termo "bug"?',
+    'O termo "bug" para descrever falhas em software surgiu quando uma mariposa foi encontrada dentro de um computador, em 1947.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual a palavra em inglês que mais repete duplas d letras?',
+    'A palavra "bookkeeper" (contador) é uma das poucas em inglês que possui três letras consecutivas duplicadas.'
+)
